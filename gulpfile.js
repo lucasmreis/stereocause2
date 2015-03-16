06d547ee0463e267ff8c6b4c3ba2f6254e41976a
Lucas Reis <lucasmreis@gmail.com>
2015-03-16 09:00:06 -0300

    gulp ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
-
-gulp.task('default', function() {
-  console.log('Testando');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+ 
+gulp.task('styles', function() {
+  return gulp.src('src/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/styles'));
 });
+
+gulp.task('template-index', function () {
+  var templateData = {
+    title: 'Stereo Cause'
+  };
+
+  return gulp.src('src/templates/index.handlebars')
+    .pipe(handlebars(templateData))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['template-index', 'styles']);
