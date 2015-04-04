b817cf21860dd7443de9055a91edc0a765031fcc
Lucas Reis <lucasmreis@gmail.com>
2015-04-04 01:17:40 -0300

    logger

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,12 @@
 import Hapi from 'hapi';
+import Good from 'good';
+import GoodConsole from 'good-console';
 
 var server = new Hapi.Server();
 
 server.connection({ port: 8080 });
 
-server.start(function() {
-    console.log('Hapi server started @', server.info.uri);
-});
-
-// STATIS FILES
+// STATIC FILES
 server.route({
     path: '/{path*}',
     method: 'GET',
@@ -19,4 +17,22 @@ server.route({
         index: true
       }
     }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: GoodConsole,
+            args:[{ log: '*', response: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
