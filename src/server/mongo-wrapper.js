6f49f3bf781bed6ba7af398135011deaff473fe9
Lucas Reis <lucasmreis@gmail.com>
2015-04-05 20:07:47 -0300

    stats api ok

---
+++ src/server/mongo-wrapper.js
@@ -0,0 +1,14 @@
+export const getDb = uri => client =>
+  new Promise((resolve, reject) => client.connect(uri,
+    (err, db) => !!err ? reject(err) : resolve(db)));
+
+export const closeDb = okAndDb => {
+  const [ok, db] = okAndDb;
+  db.close();
+  return ok;
+};
+
+export const aggregate = query => collection => db =>
+  new Promise((resolve, reject) =>
+    db.collection(collection).aggregate(query,
+      (err, ok) => !!err ? reject(err) : resolve([ok, db])));
--- src/server/routes/stats.js
+++ src/server/routes/stats.js
@@ -1,9 +1,56 @@
+import R from 'ramda';
+import Mongo from 'mongodb';
+import config from '../config';
+
+import {getDb, aggregate, closeDb} from '../mongo-wrapper';
+
+const compose = R.compose;
+const composeP = R.composeP;
+const concat = R.concat;
+const head = R.head;
+const prop = R.prop;
+const K = R.always;
+const log = m => x => { console.log(m, x); return x; };
+
+var MongoClient = Mongo.MongoClient;
+
+const query = c => [
+  {
+    $match: { cause: c }
+  },
+  {
+    $group: {
+      _id: '$cause',
+      sumArtist: { $sum: '$artist' },
+      sumCharity: { $sum: '$charity' },
+      sumStereoCause: { $sum: '$stereoCause' },
+      count: { $sum: 1  }
+    }
+  }];
+
 export default {
   method: 'GET',
 
   path: '/api/stats/{cause}',
 
   handler: function (request, reply) {
-    reply('Hello, world! Cause: ' + request.params.cause);
+    const id = compose(
+      prop('cause'),
+      prop('params'));
+
+    const q = compose(
+      query,
+      id);
+
+    const onlyRequested = head;
+
+    let response = composeP(
+      reply,
+      onlyRequested,
+      closeDb,
+      aggregate(q(request))('contributions'),
+      getDb(config.mongo.uri));
+
+    response(MongoClient);
   }
 }
